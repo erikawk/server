2e145a0ae3828822431b9b393f8df461968bf5b0
erikawk <105462871+erikawk@users.noreply.github.com>
2023-05-31 16:51:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ app.post('/dht11he', (req, res) => {
   res.send("Mensagem Enviada")
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 wss.on('connection', (ws) => {
   clients.push(ws);
   ws.on('message', (message) => {
